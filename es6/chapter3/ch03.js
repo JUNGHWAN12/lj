03880d78c8f5261c988494d753c0eef794e04c45
진정환 <wjdghks4423@gmail.com>
2025-12-10 15:15:16 +0900

    CHAPTER3 추가

---
+++ es6/chapter3/ch03.js
@@ -0,0 +1,9 @@
+let currentTempC = 22;
+console.log(currentTempC);
+currentTempC = 25;
+console.log(currentTempC);
+let targetTempC,room1 = currentTempC;
+console.log(targetTempC,room1);
+
+const ROOM_TEMP_C = 21.5, MAX_TEMP_C = 30;
+console.log(ROOM_TEMP_C,MAX_TEMP_C)
